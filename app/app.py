26aba980fc332ec900365e935f57ce196021a7bc
unknown <maurojuareza@gmail.com>
2025-07-06 18:23:22 -0600

    Se modifica app y se agrega get_ip.sh

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hola mundo desde Flask"
+@app.route('/')
+def show_ip():
+    try:
+        result = subprocess.run(
+            ["./get_ip.sh"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True
+        )
+        return f"<h1>Tu IP pública es:</h1><pre>{result.stdout}</pre>"
+    except subprocess.CalledProcessError as e:
+        return f"<h1>Error ejecutando el script</h1><pre>{e.stderr}</pre>"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ app/get_ip.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+MY_IP_ADDR=$(curl -s http://myip.enix.org/REMOTE_ADDR)
+[ "$MY_IP_ADDR" ] || {
+  echo "Sorry, I could not figure out my public IP address."
+  echo "(I use http://myip.enix.org/REMOTE_ADDR/ for that purpose.)"
+  exit 1
+}
+echo "$MY_IP_ADDR"
